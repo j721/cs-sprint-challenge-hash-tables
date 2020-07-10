2304dc63726486ffe437492efdad55be7a715606
Julia Vu <juliadvu@gmail.com>
2020-07-10 10:39:53 -0500

    made more comments and updates to ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -6,6 +6,15 @@ Do not use numpy or sets to solve this; Big Hint: (use dict or hashtables, pleas
 
 We're given a list of lists that contain integers:
 
+[
+    [1, 2, 3, 4, 5],
+    [12, 7, 2, 9, 1],
+    [99, 2, 7, 1,]
+]
+
+From the above input, the return value will be:
+
+[1, 2]
 
 And we need to compute the intersection, that is, numbers that exist in all lists.
 
@@ -19,31 +28,26 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    cache ={}
     # Your code here
-    # result = []
+   
+   #create a cache dictionary
+    cache ={}
 
-    # for array in arrays:
-    #      for i in array:
-    #          if i in cache:
-    #              cache[i] +=1
-    #          else:
-    #             cache[i] = 1
-    
-
-    # for key,value in cache.items():
-    #     if value == len(arrays):
-    #         result.append(key)
-
-      
-
+    #loop through the individual array inside of the larger array
     for array in arrays:
+        #check for element in the individual array
         for i in array:
+            #if element is in the cache then increment +=1
             if i in cache:
                 cache[i] +=1
+            #else element not in cache set the value to be 1
             else:
                 cache[i] = 1
-    result = [data[0] for data in cache.items() if data[1] == len(arrays)]
+                
+    #for each value in the cache starting the traversal from the first index
+    #if the value is equal to the length of the larger arrays 
+    # then return the result in the form of an array
+    result = [value[0] for value in cache.items() if value[1] == len(arrays)]
 
     return result
 
