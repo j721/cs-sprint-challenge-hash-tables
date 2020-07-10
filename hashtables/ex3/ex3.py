210cfeaf57fd8b87d252df91b06d38742c871d5b
Julia Vu <juliadvu@gmail.com>
2020-07-10 10:30:17 -0500

    working on ex3. test passes when cache is created inside the function and fails when cache is created outside/globally

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,49 @@
+"""
+
+Find the intersection between multiple lists of integers.
+
+Do not use numpy or sets to solve this; Big Hint: (use dict or hashtables, please). 
+
+We're given a list of lists that contain integers:
+
+
+And we need to compute the intersection, that is, numbers that exist in all lists.
+
+"""
+
+
+
+
+
 def intersection(arrays):
     """
     YOUR CODE HERE
     """
+    cache ={}
     # Your code here
+    # result = []
+
+    # for array in arrays:
+    #      for i in array:
+    #          if i in cache:
+    #              cache[i] +=1
+    #          else:
+    #             cache[i] = 1
+    
+
+    # for key,value in cache.items():
+    #     if value == len(arrays):
+    #         result.append(key)
+
+      
+
+    for array in arrays:
+        for i in array:
+            if i in cache:
+                cache[i] +=1
+            else:
+                cache[i] = 1
+    result = [data[0] for data in cache.items() if data[1] == len(arrays)]
 
     return result
 
